0c0a1c9997b3d5852d09c8eab7e5c192978c1e15
Kaori <kaorikaren1214@gmail.com>
2020-10-17 17:19:18 +0000

    Finish login-function2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = '新規作成に成功しました。'
-      redirect_to @user
+     log_in @user # 保存成功後、ログインします。
+     flash[:success] = '新規作成に成功しました。'
+     redirect_to @user
     else
-      render :new
+     render :new
     end
   end
-
+  
   private
 
     def user_params
